fad5b18fb458f3f0d91ee548243f63b23b4fe43c
unknown <dhanna@alphaworks.tech>
2022-11-06 12:19:53 -0800

    Impact Site Categories

--- home.php
+++ home.php
@@ -8,6 +8,12 @@
 ?>
 <?php get_header(); ?>
 <?php get_template_part('template-parts/hero')?>
-
+<?php get_template_part('template-parts/cybersec')?>
+<?php get_template_part('template-parts/possible-solutions')?>
+<?php get_template_part('template-parts/resources')?>
+<?php get_template_part('template-parts/mission-state')?>
+<?php get_template_part('template-parts/meet-team')?>
+<?php get_template_part('template-parts/contact')?>
+<?php get_template_part('template-parts/branding')?>
 
 <?php get_footer(); ?>
