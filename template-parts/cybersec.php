914358950df53eaea42d12a2ce9e939b98a2bd20
unknown <dhanna@alphaworks.tech>
2022-11-16 17:05:48 -0800

    Installed Cybersec and Possible-Solutions

--- template-parts/cybersec.php
+++ template-parts/cybersec.php
@@ -1,4 +1,7 @@
-<section>
-<h2>What is Cyber Security?</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<section class="CyberSection">
+    <h2>What is Cyber Security?</h2>
+    <div class="cyberseccopy">
+        <img src="http://teamde.local/wp-content/uploads/2022/11/WhatIsCybersecurityImg.jpg" width="200px" height="200px">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
 </section>
