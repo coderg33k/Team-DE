0221cf50265656a676bf96e850f8dfbdbc7851e5
unknown <dhanna@alphaworks.tech>
2022-11-21 16:08:12 -0800

    Updated Resource Section

--- template-parts/resources.php
+++ template-parts/resources.php
@@ -1,4 +1,7 @@
-<section>
-<h2>Resources</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<section class="ResourceSection">
+    <h2>Resources</h2>
+    <div class="resourcecopy">
+        <img src="http://teamde.local/wp-content/uploads/2022/11/Cybersecurity-Resources-e1669051939949.jpg" width="200px" height="200px">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
 </section>
