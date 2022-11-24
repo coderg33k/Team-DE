ed01fb1a95ca59531de831e7bad0e9368cee47d8
unknown <dhanna@alphaworks.tech>
2022-11-23 18:01:16 -0800

    Modified Template Parts

--- template-parts/possible-solutions.php
+++ template-parts/possible-solutions.php
@@ -2,6 +2,6 @@
     <h2>Possible Solutions</h2>
     <div class="solutionseccopy">
         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        <img src="http://teamde.local/wp-content/uploads/2022/11/Solutions.jpg" width="200px" height="200px">
+        <img src="http://teamde.local/wp-content/uploads/2022/11/Solutions1.jpg" width="auto">
     </div>
 </section>
