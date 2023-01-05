f463354fbee5a5afaf69a0dde90a3286a479c086
unknown <dhanna@alphaworks.tech>
2023-01-05 15:08:00 -0800

    Updating Cards

--- template-parts/possible-solutions.php
+++ template-parts/possible-solutions.php
@@ -5,3 +5,92 @@
         <img src="http://teamde.local/wp-content/uploads/2022/11/Solutions1.jpg" width="auto">
     </div>
 </section>
+<section class="resourcecard">
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+</section>
--- template-parts/resources.php
+++ template-parts/resources.php
@@ -1,51 +1,51 @@
 <section class="ResourceSection">
-    <h2>Resources</h2>
-    <div class="resourcecopy">
-        <img src="http://teamde.local/wp-content/uploads/2022/11/Cybersecurity-Resources1.jpg" width="auto">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
-    </section>
-    <section class="resourcecard">
-        <div class="container">
-        <div class="card" style="width:400px">
-    <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
-    <div class="card-body">
-      <h4 class="card-title">Title</h4>
-      <p class="card-text">Description: </p>
-      <a href="#" class="btn btn-primary stretched-link">Learn More</a>
-    </div>
-    </div>
-    </div>
-
-        <div class="container">
-    <div class="card" style="width:400px">
-    <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
-    <div class="card-body">
-      <h4 class="card-title">Title</h4>
-      <p class="card-text">Description: </p>
-      <a href="#" class="btn btn-primary stretched-link">Learn More</a>
-    </div>
-    </div>
-    </div>
-
-    <div class="container">
-    <div class="card" style="width:400px">
-    <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
-    <div class="card-body">
-      <h4 class="card-title">Title</h4>
-      <p class="card-text">Description: </p>
-      <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+  <h2>Resources</h2>
+  <div class="resourcecopy">
+    <img src="http://teamde.local/wp-content/uploads/2022/11/Cybersecurity-Resources1.jpg" width="auto">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</section>
+<section class="resourcecard">
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
     </div>
   </div>
-</div>
-<div class="container">
-    <div class="card" style="width:400px">
-    <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
-    <div class="card-body">
-      <h4 class="card-title">Title</h4>
-      <p class="card-text">Description: </p>
-      <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
     </div>
   </div>
-</div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="container">
+    <div class="card" style="width:300px">
+      <!-- <img class="card-img-top" src="img_avatar1.png" alt="Card image" style="width:100%"> -->
+      <div class="card-body">
+        <h4 class="card-title">Title</h4>
+        <p class="card-text">Description: </p>
+        <a href="#" class="btn btn-primary stretched-link">Learn More</a>
+      </div>
+    </div>
+  </div>
 </section>
