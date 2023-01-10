e7615bffdf5915ff5f514cfdc1f716790437380c
Edwin <99057140+EdwinRodriguezcode@users.noreply.github.com>
2023-01-10 09:48:12 -0800

    redo

--- template-parts/meet-team.php
+++ template-parts/meet-team.php
@@ -1,16 +1,15 @@
 <div class="team-card-container">
-<div class="main-card" style="width: 18rem;">
-  <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="..." style:"height: 100%;width: 100%; ,border: 3px solid #ffff;, border-radius: 50%;, object-fit: cover;">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="main-card" style="width: 18rem;">
+    <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="..." style:"height: 100%;width: 100%; ,border: 3px solid #ffff;, border-radius: 50%;, object-fit: cover;">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  <div class="main-card" style="width: 18rem;">
+    <!-- image goes here upload image thru dash -->
+    <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
   </div>
 </div>
-<div class="main-card" style="width: 18rem;">
-<!-- image goes here upload image thru dash -->
-  <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="...">
-  <div class="card-body">
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-</div>
-	
