a13ee156983db79dda82cca5bb4a46a6d381d87a
unknown <dhanna@alphaworks.tech>
2023-01-12 11:40:28 -0800

    Manually Merged Files

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * The header for our theme
  *
@@ -12,40 +11,56 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
-
+<html <?php language_attributes(); ?>
 <head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1"/>
 	<link rel="profile" href="https://gmpg.org/xfn/11">
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
-	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+	<link rel='preload' href='https://static.everyaction.com/ea-actiontag/at.js' as='script' crossorigin='anonymous'>
+ 	<link rel='preload' href='https://static.everyaction.com/ea-actiontag/at.min.css' as='style'>
+ 	<script type='text/javascript' src='https://static.everyaction.com/ea-actiontag/at.js' crossorigin='anonymous'></script>
+ 	<link rel='preload' href='https://nvlupin.blob.core.windows.net/designs/CustomStylesheet_4dc81aac85fe05fa537ab6df61721eebf6e1b6701722d0a9270b0a66ce090d51.css' as='style'>
+	 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  	<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.slim.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+	 <script src="https://kit.fontawesome.com/6b35074062.js" crossorigin="anonymous"></script>
 	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-	<?php wp_body_open(); ?>
-	<div id="page" class="site">
-		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'teamde'); ?></a>
-
-		<header id="masthead" class="site-header">
-			<div class="site-branding">
-				<?php
-				the_custom_logo();
-
-
-				// <nav id="site-navigation" class="main-navigation">
-				// <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'teamde' ); 
-				?></button>
-				<?php
-				wp_nav_menu(
-					array(
-						'theme_location' => 'menu-1',
-						'menu_id'        => 'primary-menu',
-					)
-				);
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'Team-DE' ); ?></a>
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo(); 
 				?>
-				</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"> -->
+
+			<input type="checkbox" id="checkbox3" aria-controls="primary-menu" aria-expanded="false" class="checkbox3 visuallyHidden menu-toggle">
+        <label for="checkbox3">
+            <div class="hamburger hamburger3">
+                <span class="bar bar1"></span>
+                <span class="bar bar2"></span>
+                <span class="bar bar3"></span>
+                <span class="bar bar4"></span>
+            </div>
+        </label>
+	
+
+			<!-- </button> -->
+			
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+	</header><!-- #masthead -->
--- template-parts/meet-team.php
+++ template-parts/meet-team.php
@@ -1,4 +1,16 @@
-<section>
-<h2>Meet the Team</h2>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-</section>
+<div class="team-card-container">
+  <div class="main-card" style="width: 18rem;">
+    <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="..." style:"height: 100%;width: 100%; ,border: 3px solid #ffff;, border-radius: 50%;, object-fit: cover;">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+  <div class="main-card" style="width: 18rem;">
+    <!-- image goes here upload image thru dash -->
+    <img src="http://localhost:10197/wp-content/uploads/2023/01/T03A6AQRX-U030QA5PTHQ-3f41af5a32ac-48.jpg" class="card-img-top" alt="...">
+    <div class="card-body">
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    </div>
+  </div>
+</div>
+
