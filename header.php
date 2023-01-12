8e5ca84c66b0b4ff54d942c3dd34043eb7b09de7
Edwin <99057140+EdwinRodriguezcode@users.noreply.github.com>
2023-01-12 11:42:45 -0800

    merge

--- header.php
+++ header.php
@@ -11,7 +11,7 @@
 
 ?>
 <!doctype html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
@@ -28,12 +28,12 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
 <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'Team-DE' ); ?></a>
 	<header id="masthead" class="site-header">
-		<div style="height=10px width=10px" class="site-branding">
+		<div  class="site-branding">
 			<?php
 			the_custom_logo(); 
 				?>
